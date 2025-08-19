560b130a55ce5c7879d4e98fd092f7c0e055e634
cloudwithrk <cloudwithrk@gmail.com>
2025-08-19 09:54:59 +0530

    Changed

---
+++ backend/config/config/config.js
@@ -0,0 +1,25 @@
+require("dotenv").config(); // load .env file
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASS || "root",
+    database: process.env.DB_NAME || "school_erp_db",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: process.env.DB_DIALECT || "mysql"
+  },
+  test: {
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASS || "root",
+    database: process.env.DB_NAME_TEST || "school_erp_db_test",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: process.env.DB_DIALECT || "mysql"
+  },
+  production: {
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASS || "root",
+    database: process.env.DB_NAME || "school_erp_db",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: process.env.DB_DIALECT || "mysql"
+  }
+};
