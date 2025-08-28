e1f89fdd217ff131e4aa60e4fb34d8d5269eb515
cloudwithrk <cloudwithrk@gmail.com>
2025-08-28 15:11:07 +0530

    Changed in k8s mysql service

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,21 +3,21 @@ require("dotenv").config(); // load .env file
 module.exports = {
   development: {
     username: process.env.DB_USER || "root",
-    password: process.env.DB_PASS || "root",
+    password: process.env.DB_PASSWORD || "root",
     database: process.env.DB_NAME || "school_erp_db",
     host: process.env.DB_HOST || "127.0.0.1",
     dialect: process.env.DB_DIALECT || "mysql"
   },
   test: {
     username: process.env.DB_USER || "root",
-    password: process.env.DB_PASS || "root",
+    password: process.env.DB_PASSWORD || "root",
     database: process.env.DB_NAME_TEST || "school_erp_db_test",
     host: process.env.DB_HOST || "127.0.0.1",
     dialect: process.env.DB_DIALECT || "mysql"
   },
   production: {
     username: process.env.DB_USER || "root",
-    password: process.env.DB_PASS || "root",
+    password: process.env.DB_PASSWORD || "root",
     database: process.env.DB_NAME || "school_erp_db",
     host: process.env.DB_HOST || "127.0.0.1",
     dialect: process.env.DB_DIALECT || "mysql"
